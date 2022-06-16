e239851e74470bc20279fe675f755940c18ba182
Theo Petit <theo.p83@gmail.com>
2022-06-16 18:43:33 +0200

    feat: details invisible when no week selected

--- frontend/src/details/Details.svelte
+++ frontend/src/details/Details.svelte
@@ -2,7 +2,14 @@
     export let pos;
     let display = { x: "", y: "" };
 
+    let details;
+
     function updateDisplay() {
+        if (details) {
+            details.style.visibility =
+                pos.x + pos.y >= 0 ? "visible" : "hidden";
+        }
+
         display.x = pos.x >= 0 ? String(pos.x) : "";
         display.y = pos.y >= 0 ? String(pos.y) : "";
     }
@@ -10,7 +17,9 @@
     $: pos, updateDisplay();
 </script>
 
-<div class="details"><h1>{display.x}<br />{display.y}</h1></div>
+<div bind:this={details} class="details">
+    <h1>{display.x}<br />{display.y}</h1>
+</div>
 
 <style>
     .details {
